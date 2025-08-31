31ba2e5b1e2991e0ecd56a592678c4a31a2c2aba
ruuxi <lolruuxi@gmail.com>
2025-08-30 23:31:29 -0700

    main

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import DesktopShell from "@/components/desktop/DesktopShell";
 
 export default function Home() {
   return (
-    <main className="p-2 sm:p-4">
+    <main>
       <DesktopShell />
     </main>
   );
